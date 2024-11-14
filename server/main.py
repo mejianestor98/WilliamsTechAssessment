27234665c09a89c788aded24770dc051f8033e46
Nestor Mejia <nestor.mejia98@gmail.com>
2024-11-14 23:46:48 +0000

    feat: serve bundled frontend

--- server/main.py
+++ server/main.py
@@ -2,6 +2,8 @@ import uvicorn
 from ariadne import make_executable_schema, load_schema_from_path
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from ariadne.asgi import GraphQL
 
 from resolvers import resolvers
@@ -17,15 +19,16 @@ app = FastAPI()
 app.add_middleware(CORSMiddleware,
                    allow_origins=['*'],
                    allow_credentials=True,
-                   allow_methods=["*"],
-                   allow_headers=["*"])
+                   allow_methods=['*'],
+                   allow_headers=['*'])
 
 app.mount('/graphql', graphql_app)
+app.mount('/assets', StaticFiles(directory='./bundled_ui/assets'), name='assets')
 
 
-@app.get('/')  # will serve built frontend
+@app.get('/')
 def home():
-    return 'Welcome to the FastAPI and GraphQL backend!'
+    return FileResponse('./bundled_ui/index.html')
 
 
 if __name__ == '__main__':
