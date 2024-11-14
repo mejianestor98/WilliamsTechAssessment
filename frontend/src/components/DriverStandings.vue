b83fd67d56f556662353a7164e6ae8a7acb8cc57
Nestor Mejia <nestor.mejia98@gmail.com>
2024-11-14 22:49:08 +0000

    feat: driver details on click

--- frontend/src/components/DriverStandings.vue
+++ frontend/src/components/DriverStandings.vue
@@ -1,19 +1,40 @@
 <script setup>
+import gql from 'graphql-tag'
 import { computed, ref, watch } from 'vue';
-import { useQuery } from '@vue/apollo-composable';
-import { RACES_QUERY } from '@/graphql/standings.js';
+import { useQuery, useLazyQuery } from '@vue/apollo-composable';
+import { RACES_QUERY, DRIVER_STANDINGS_QUERY } from '@/graphql/standings.js';
 
 const { result, loading, error } = useQuery(RACES_QUERY);
 
 const races = computed(() => result.value?.races ?? []);
 const selectedRaceId = ref(null);
 
-watch(selectedRaceId, (newVal, oldVal) => {
-    console.log(`Selected race ID changed from ${oldVal} to ${newVal}`);
+const fetchDriverStandings = ref(false);
+const { result: driverStandingsResult, refetch: loadDriverStandings } = useQuery(DRIVER_STANDINGS_QUERY,
+                                                                                 { raceId: selectedRaceId },
+                                                                                 { enabled: fetchDriverStandings });
+const driverStandings = computed(() => {
+    return (driverStandingsResult.value?.driver_standings ?? [])
+            .slice()
+            .sort((a, b) => a.position - b.position);
 });
 
-</script>
+watch(selectedRaceId, (newVal, oldVal) => {
+    console.log(`Selected race ID changed from ${oldVal} to ${newVal}`);
+    if (newVal) {
+        clickedDriver.value = null;
+        fetchDriverStandings.value = true;
+        loadDriverStandings({ raceId: newVal });
+    }
+});
 
+const clickedDriver = ref(null)
+const handleDriverClick = (driver) => {
+    console.log(driver)
+    clickedDriver.value = driver
+};
+
+</script>
 
 <template>
     <div class="container-fluid">
@@ -21,11 +42,45 @@ watch(selectedRaceId, (newVal, oldVal) => {
             <div class="col-2">
                 <select class="form-select" aria-label="Default select example" v-model="selectedRaceId">
                     <option selected>Select Race</option>
-                    <option v-for="race in races" :key="race.id" :value="race.id">{{ race.year }} - {{ race.name }}</option>
+                    <option v-for="race in races" :key="race.id" :value="race.id">{{ race.year }} - {{ race.name }}
+                    </option>
                 </select>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-6">
+                <table class="table table-dark table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">Pos</th>
+                            <th scope="col">Driver</th>
+                            <th scope="col">Points</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="driverStanding in driverStandings"
+                            :key="driverStanding.driver.id"
+                            @click="handleDriverClick(driverStanding.driver)">
+                            <th scope="row">{{ driverStanding.position }}</th>
+                            <td>{{ driverStanding.driver.forename }} {{ driverStanding.driver.surname }}</td>
+                            <td>{{ driverStanding.points }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-6" v-if="clickedDriver">
+                <h1>{{ clickedDriver.surname }}</h1>
+                <h3>{{ clickedDriver.forename }}</h3>
+                <h2>{{ clickedDriver.code }}</h2>
+                <p> {{ clickedDriver.date_of_birth }} - {{ clickedDriver.nationality }}</p>
+                <a :href="clickedDriver.url" target="_blank">Wikipedia page</a>
             </div>
         </div>
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+tr {
+    cursor: pointer;
+}
+</style>
--- frontend/src/graphql/standings.js
+++ frontend/src/graphql/standings.js
@@ -1,4 +1,4 @@
-import gql from "graphql-tag";
+import gql from 'graphql-tag';
 
 export const RACES_QUERY = gql`
 query {
@@ -7,5 +7,21 @@ query {
         year
         name
     }
-}
-`;
+}`;
+
+export const DRIVER_STANDINGS_QUERY = gql`
+query GetDriverStandings($raceId: Int){
+    driver_standings (race_id: $raceId){
+        driver {
+            id
+            forename
+            surname
+            date_of_birth
+            nationality
+            code
+            url
+        }
+        points
+        position
+    }
+}`;
